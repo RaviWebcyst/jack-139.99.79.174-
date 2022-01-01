983470e7281fb1b6ae193356b87315d0f376ce67
Aram <aram@claypotcreative.com>
2021-12-31 17:19:01 -0700

    new slave form added

--- components/SlaveManager.js
+++ components/SlaveManager.js
@@ -1,7 +1,47 @@
 export default function SlaveManager() {
   return (
-    <div>
+    <div className="w3-container">
       <h1>Slave Manager</h1>
+
+      <form id="new-slave-form" className="w3-center w3-border">
+        <h1>New Slave</h1>
+        <label htmlFor="name">Name</label>
+        <br />
+        <input
+          name="name"
+          className="w3-center w3-input w3-border w3-animate-input "
+          type="text"
+          style={{ width: "50%", textAlign: "center", display: "inline-block" }}
+        />
+        <br />
+        <label htmlFor="binance-key">Binance API Key</label>
+        <br />
+        <input
+          name="binance-key"
+          className="w3-input w3-border w3-animate-input"
+          type="text"
+          style={{ width: "50%", textAlign: "center", display: "inline-block" }}
+        />
+        <br />
+        <label htmlFor="binance-secret">Binance API Secret</label>
+        <br />
+        <input
+          name="binance-secret"
+          className="w3-input w3-border w3-animate-input"
+          type="text"
+          style={{ width: "50%", textAlign: "center", display: "inline-block" }}
+        />
+        <br />
+
+        <input
+          name="binance-secret"
+          className="w3-input w3-border w3-hoverable"
+          type="submit"
+          style={{ width: "20%", textAlign: "center", display: "inline-block" }}
+        />
+      </form>
+
+      <br />
     </div>
   );
 }
